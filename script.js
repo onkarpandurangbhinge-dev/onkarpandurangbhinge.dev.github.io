047182de9ab405e87851cbb4068c9c72d1d3be16
onkar bhinge <onkarpandurangbhinge@gmail.com>
2026-03-10 19:19:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
+let cart=[];
+
+function addToCart(item,price){
+
+cart.push({item:item,price:price});
+
+localStorage.setItem("cart",JSON.stringify(cart));
+
+alert(item+" added to cart");
+
+}
+
+function showCart(){
+
+let data=JSON.parse(localStorage.getItem("cart")) || [];
+
+let output="";
+let total=0;
+
+for(let i=0;i<data.length;i++){
+
+output+=data[i].item+" - ₹"+data[i].price+"<br>";
+
+total+=data[i].price;
+
+}
+
+document.getElementById("cartItems").innerHTML=output;
+
+document.getElementById("total").innerHTML="Total: ₹"+total;
+
+}
+
+function placeOrder(){
+
+alert("Order Placed Successfully!");
+
+localStorage.removeItem("cart");
+
+location.reload();
+
+}
+
+function toggleDarkMode(){
+
+document.body.classList.toggle("dark-mode");
+
+}
+
 function searchFood(){
 
-let input = document.getElementById("search").value.toLowerCase();
-let cards = document.getElementsByClassName("card");
+let input=document.getElementById("searchBar").value.toLowerCase();
+let cards=document.getElementsByClassName("card");
 
 for(let i=0;i<cards.length;i++){
 
-let title = cards[i].innerText.toLowerCase();
+let text=cards[i].innerText.toLowerCase();
 
-if(title.includes(input)){
+if(text.includes(input)){
 cards[i].style.display="block";
-}
-else{
+}else{
 cards[i].style.display="none";
 }
 
